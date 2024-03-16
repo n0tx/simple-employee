de403e71eb7aca8a7969cddb9496ef2df277db45
n0tx <rcandra91@msn.com>
2024-03-17 00:01:51 +0700

    submit the updated employee data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,4 @@ $routes->get('helloworld', 'HelloWorld::index');
 $routes->get('/', 'EmployeeController::index');
 $routes->post('add', 'EmployeeController::add');
 $routes->get('edit/(:num)', 'EmployeeController::edit/$1');
+$routes->post('update', 'EmployeeController::update');
--- app/Controllers/EmployeeController.php
+++ app/Controllers/EmployeeController.php
@@ -3,12 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\EmployeeModel;
 
 class EmployeeController extends BaseController
 {
     public function index()
     {
-        $employeeModel = new \App\Models\EmployeeModel();
+        $employeeModel = new EmployeeModel();
         $data['title'] = 'Simple Employee Management';
         $data['employees'] = $employeeModel->findAll();
 
@@ -17,7 +18,7 @@ class EmployeeController extends BaseController
 
     public function add()
     {
-        $employeeModel = new \App\Models\EmployeeModel();
+        $employeeModel = new EmployeeModel();
         $data = [
             'name' => $this->request->getPost('name'),
             'gender' => $this->request->getPost('gender'),
@@ -34,11 +35,29 @@ class EmployeeController extends BaseController
     }
 
     public function edit($id = null) {
-        $employeeModel = new \App\Models\EmployeeModel();
+        $employeeModel = new EmployeeModel();
         $data['title'] = 'Simple Employee Management';
         $data['employee'] = $employeeModel->find($id);
 
         return view('employee_edit', $data);
+    }
+
+    public function update() {
+        $employeeModel = new EmployeeModel();
+        $id = $this->request->getPost('id');
+        $data = [
+            'name' => $this->request->getPost('name'),
+            'gender' => $this->request->getPost('gender'),
+            'age' => $this->request->getPost('age'),
+            'first_vaccine' => $this->request->getPost('first_vaccine'),
+            'second_vaccine' => $this->request->getPost('second_vaccine')
+        ];
+        $employeeModel->update($id, $data);
+        echo '
+        <script>
+            alert("Employee Data Updated");
+            window.location="'.base_url('/').'";
+        </script>';
     }
 
 
