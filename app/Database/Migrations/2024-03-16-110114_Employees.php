97dff30f2ead894a1b2b56419a67d93c816713f7
n0tx <rcandra91@msn.com>
2024-03-16 20:24:38 +0700

    Add employees table fields php spark migrate

--- app/Database/Migrations/2024-03-16-110114_Employees.php
+++ app/Database/Migrations/2024-03-16-110114_Employees.php
@@ -8,11 +8,47 @@ class Employees extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'gender' => [
+                'type' => 'ENUM',
+                'constraint' => ['Male', 'Female'],
+                'default' => 'Male',
+            ],
+            'age' => [
+                'type' => 'INT',
+                'constraint' => 2,
+            ],
+            'first_vaccine' => [
+                'type' => 'ENUM',
+                'constraint' => ['already', 'not yet'],
+                'default' => 'not yet',
+
+            ],
+            'second_vaccine' => [
+                'type' => 'ENUM',
+                'constraint' => ['already', 'not yet'],
+                'default' => 'not yet',
+
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('employees');
+        
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('employees');
     }
 }
